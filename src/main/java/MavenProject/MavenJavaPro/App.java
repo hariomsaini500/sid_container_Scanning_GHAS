2270d53cae43ee1025030d72b0ef24e460296e1e
Sid119 <45572023+Sid119@users.noreply.github.com>
2024-01-24 14:29:25 +0530

    Update App.java

--- src/main/java/MavenProject/MavenJavaPro/App.java
+++ src/main/java/MavenProject/MavenJavaPro/App.java
@@ -8,10 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        var sonarToken;
-        sonarToken = "sqp_bf5060fe9f5f52d00008d817fe7300638cf063a1";
-        var snykToken;
-        snykToken = "1a0e39cd-c849-48d2-8ce2-2c408ce0011c";
+
         System.out.println( "Hello World!" );
     }
 }
